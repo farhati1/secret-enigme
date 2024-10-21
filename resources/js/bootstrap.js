49130ab0130a9f02382111c8807ceebdd2fd3c4b
farhati1 <farhati.m@outlook.fr>
2024-10-21 15:45:16 +0200

    fix problem xsrftoken

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.axios.defaults.withCredentials = true; // without this one it wasn't working in Firefox
+window.axios.defaults.withXSRFToken = true;
+
